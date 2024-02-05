8e85632daeb9c680ccfb72ae4335592290c5015b
Monbirr <156954801+MOnbirr@users.noreply.github.com>
2024-02-05 05:24:37 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,10 +24,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "bda69b2d94259a62c8374769b8373c49
 "icons/android-chrome-192x192.png": "b651053cc0449b185fa4854c0774da97",
 "icons/android-chrome-512x512.png": "48925910dc7b68ff2abe72fb953b090f",
 "icons/apple-touch-icon.png": "ab909c2b03c2c453bee166497f31f17e",
-"index.html": "834eb1c6236bc952770cc814a6514488",
-"/": "834eb1c6236bc952770cc814a6514488",
+"index.html": "69d0ab868d7081413da6cc1c58a1c659",
+"/": "69d0ab868d7081413da6cc1c58a1c659",
 "logo.png": "b651053cc0449b185fa4854c0774da97",
-"main.dart.js": "8da7143d1daeeb26f8ddf7afa406388f",
+"main.dart.js": "2e253d92da308ee53de3793e4114dde6",
 "manifest.json": "8a2c506ab479ab63fb35ec0702c5eb66",
 "monb.png": "bca00b0bb3594874bc3b16613bb53dbd",
 "version.json": "26f8f4c0926b21a96e17f5d6ec8768e9"};
